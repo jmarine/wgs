fbbc6edb1e70f7d726f8dfadb1a5d86e17b5ee40
Jordi Marine Fort <jmarine@tinet.org>
2012-08-26 12:40:43 +0200

    JVM switching with JAVA_HOME and JAR name changes

--- WampServicesRT/run.sh
+++ WampServicesRT/run.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-/opt/jdk1.7.0/bin/java -Djava.util.logging.config.file=logging.properties -jar dist/WampServices1.jar wampservices.properties
+export JAVA=java
+if [ ${JAVA_HOME} ]; then
+  export JAVA=$JAVA_HOME/bin/java
+fi
+
+$JAVA -Djava.util.logging.config.file=logging.properties -jar dist/WampServicesRT.jar wampservices.properties
