dca1cc22433270bdba627f10b1dde2405e28523f
Jordi Marine Fort <jmarine@tinet.org>
2012-11-02 19:51:25 +0100

    Allow options on topic subscription.

--- WampServicesRT/src/com/github/jmarine/wampservices/WampSubscriptionOptions.java
+++ WampServicesRT/src/com/github/jmarine/wampservices/WampSubscriptionOptions.java
@@ -5,17 +5,42 @@ import org.codehaus.jackson.JsonNode;
 
 public class WampSubscriptionOptions 
 {
+    private boolean eventsEnabled;
+    private boolean metaEventsEnabled;
+    private boolean eventIdRequested;
     private boolean publisherIdRequested;
     private int     numberEventsToSendOnSubscription;
+    private long    sinceTimeEventsToSendOnSubscription;
 
     public WampSubscriptionOptions(JsonNode node) {
+        this.eventsEnabled = true;
+        // this.sinceTimeEventsToSendOnSubscription = System.currentTimeMillis();
+        
         if(node != null) {
+            if(node.has("events")) {
+                setEventsEnabled(node.get("events").asBoolean(true));
+            }     
+            
+            if(node.has("metaevents")) {
+                setMetaEventsEnabled(node.get("metaevents").asBoolean(false));
+            }          
+            
+            if(node.has("eventId")) {
+                setEventIdRequested(node.get("eventId").asBoolean(false));
+            }
+            
             if(node.has("identifyEventPublisher")) {
                 setPublisherIdRequested(node.get("identifyEventPublisher").asBoolean(false));
             }
+            
             if(node.has("last")) {
                 setNumberEventsToSendOnSubscription(node.get("last").asInt(0));
             }
+            
+            if(node.has("sinceTime")) {
+                setSinceTimeEventsToSendOnSubscription(node.get("sinceTime").asLong(0));
+            }            
+              
         }
     }
 
@@ -35,6 +60,37 @@ public class WampSubscriptionOptions
     public void setNumberEventsToSendOnSubscription(int numberEventsToSendOnSubscription) {
         this.numberEventsToSendOnSubscription = numberEventsToSendOnSubscription;
     }
-    
+
+    public boolean isEventsEnabled() {
+        return eventsEnabled;
+    }
+
+    public void setEventsEnabled(boolean eventsEnabled) {
+        this.eventsEnabled = eventsEnabled;
+    }
+
+    public boolean isMetaEventsEnabled() {
+        return metaEventsEnabled;
+    }
+
+    public void setMetaEventsEnabled(boolean metaEventsEnabled) {
+        this.metaEventsEnabled = metaEventsEnabled;
+    }
+
+    public boolean isEventIdRequested() {
+        return eventIdRequested;
+    }
+
+    public void setEventIdRequested(boolean eventIdRequested) {
+        this.eventIdRequested = eventIdRequested;
+    }
+
+    public long getSinceTimeEventsToSendOnSubscription() {
+        return sinceTimeEventsToSendOnSubscription;
+    }
+
+    public void setSinceTimeEventsToSendOnSubscription(long sinceTimeEventsToSendOnSubscription) {
+        this.sinceTimeEventsToSendOnSubscription = sinceTimeEventsToSendOnSubscription;
+    }
     
 }
