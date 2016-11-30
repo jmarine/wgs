ca31a6a2f848f6286f13ee7d2c3fd37dfe86230e
Jordi Mariné Fort <jmarine@tinet.org>
2016-11-30 20:58:53 +0100

    Maven migration.

--- WgsAPI/run_federated_8082.sh
+++ WgsAPI/run_federated_8082.sh
@@ -5,3 +5,7 @@ if [ ${JAVA_HOME} ]; then
 fi
 
 $JAVA -Xmx128m -Djava.util.logging.config.file=logging.properties -jar target/WgsAPI-2.0-SNAPSHOT.jar wgs_federated_8082.properties
+
+# Maven command:
+# $MAVEN_HOME/bin/mvn exec:exec -Dexec.executable=java -Dexec.mainClass="org.wgs.util.Server" -Dexec.args="-Xmx128m -Djava.util.logging.config.file=logging.properties -cp %classpath org.wgs.util.Server wgs_federated_8082.properties"
+
--- WgsAPI/run_standalone_8082.sh
+++ WgsAPI/run_standalone_8082.sh
@@ -4,4 +4,7 @@ if [ ${JAVA_HOME} ]; then
   export JAVA=$JAVA_HOME/bin/java
 fi
 
-$JAVA -Xmx128m -Djava.util.logging.config.file=logging.properties -Dderby.drda.host=127.0.0.1 -Dderby.drda.portNumber=15270 -jar dist/WgsAPI.jar wgs_standalone_8082.properties
+$JAVA -Xmx128m -Djava.util.logging.config.file=logging.properties -Dderby.drda.host=127.0.0.1 -Dderby.drda.portNumber=15270 -jar target/WgsAPI-2.0-SNAPSHOT.jar wgs_standalone_8082.properties
+
+# Maven command
+# $MAVEN_HOME/bin/mvn exec:exec -Dexec.executable=java -Dexec.mainClass="org.wgs.util.Server" -Dexec.args="-Xmx128m -Djava.util.logging.config.file=logging.properties -Dderby.drda.host=127.0.0.1 -Dderby.drda.portNumber=15270 -cp %classpath org.wgs.util.Server wgs_standalone_8082.properties"
