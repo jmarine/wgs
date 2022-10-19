4ff932c0dd7f2602cdaf058ab86edf8ce09f78c7
Jordi Mariné Fort <jmarine@tinet.org>
2022-10-19 16:31:34 +0200

    Migration to JDK 17 and Jakarta EE 9.1

--- WgsWebApp/src/main/java/org/wgs/sample/WgsApplicationConfig.java
+++ WgsWebApp/src/main/java/org/wgs/sample/WgsApplicationConfig.java
@@ -4,9 +4,9 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.websocket.Endpoint;
-import javax.websocket.server.ServerApplicationConfig;
-import javax.websocket.server.ServerEndpointConfig;
+import jakarta.websocket.Endpoint;
+import jakarta.websocket.server.ServerApplicationConfig;
+import jakarta.websocket.server.ServerEndpointConfig;
 
 import org.wgs.wamp.WampApplication;
 import org.wgs.wamp.transport.http.websocket.WampEndpoint;
